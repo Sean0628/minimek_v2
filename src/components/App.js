8adaaf9ad6d589418f461b89ea34c097e772cafb
Sho Ito <i.sho.0628@gmail.com>
2019-05-05 16:54:36 +0900

    add hard coded menu components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import {
+  Header,
+  Container,
+  Menu
+} from 'semantic-ui-react';
 
 import SampleComponent from '../components/SampleComponent';
 import '../styles/App.css';
@@ -10,6 +14,14 @@ const App = () => {
       <div className='App-header'>
         <Header inverted as='h1'>Project Mini-Mek</Header>
       </div>
+      <Container>
+        <Menu tabular size='massive'>
+          <Menu.Item name='unitInfo' active={true}>Unit Info</Menu.Item>
+          <Menu.Item name='pilots' active={false}>Pilots</Menu.Item>
+          <Menu.Item name='mechs' active={false}>Mechs</Menu.Item>
+          <Menu.Item name='unitOrganization' active={false}>Unit Organaization</Menu.Item>
+        </Menu>
+      </Container>
     </div>
   );
 }
